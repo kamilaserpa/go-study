4519f2b2f2b7689b8a11118ec508c74717331d39
kamilaserpa <kamilardg@gmail.com>
2025-07-14 17:06:51 -0300

    03.02 Transferencia entre contas

--- main.go
+++ main.go
@@ -28,6 +28,16 @@ func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
 	}
 }
 
+func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
+	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
+		c.saldo -= valorDaTransferencia
+		contaDestino.Depositar(valorDaTransferencia)
+		return true
+	} else {
+		return false
+	}
+}
+
 func main() {
 	contaDaSilvia := ContaCorrente{}
 	contaDaSilvia.titular = "Silvia"
@@ -41,5 +51,12 @@ func main() {
 	fmt.Println(contaDaSilvia.Depositar(-200))
 	status, valor := contaDaSilvia.Depositar(2000)
 	fmt.Println(status, "Saldo:", valor)
+
+	contaDoGustavo := ContaCorrente{titular: "Gustavo", saldo: 100}
+	statusTransferencia := contaDaSilvia.Transferir(200, &contaDoGustavo)
+
+	fmt.Println(statusTransferencia)
+	fmt.Println(contaDaSilvia)
+	fmt.Println(contaDoGustavo)
 
 }
