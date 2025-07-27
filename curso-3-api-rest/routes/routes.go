dde897720c907984788bd229740ced1062208144
kamilaserpa <kamilardg@gmail.com>
2025-07-27 14:20:38 -0300

    C3 04 Middleware - Content-type

--- curso-3-api-rest/routes/routes.go
+++ curso-3-api-rest/routes/routes.go
@@ -7,15 +7,19 @@ import (
 	"github.com/gorilla/mux"
 
 	"github.com/kamilaserpa/go-study/controllers"
+	"github.com/kamilaserpa/go-study/middleware"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
+
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("GET")
 	r.HandleFunc("/api/personalidades/{id}", controllers.GetPersonalidadePorId).Methods("GET")
 	r.HandleFunc("/api/personalidades", controllers.CriaNovaPersonalidade).Methods("POST")
 	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaPersonalidade).Methods("DELETE")
 	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("PUT")
+
 	log.Fatal(http.ListenAndServe(":8000", r)) // Qualquer problema na inicialização será logado
 }
